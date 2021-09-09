f460dae61c2318bbb513cac9c8884195f4b54960
aleksey <arashamph@gmail.com>
2021-09-09 12:58:04 +0300

    edit easyboot

--- cube/f103_easyboot/readhex.c
+++ cube/f103_easyboot/readhex.c
@@ -32,36 +32,45 @@ typedef unsigned char uint8_t;
 int main()
 {
 	FILE *fp1; char nameoffile1 [] = "f103_flash.hex";//указатель на файл
-	char buffer1 [48]; char buffer2 [48]; char buffer3 [48];//char * ptr = buffer;
-	uint8_t id = 0, length = 0; unsigned int ext_adress = 0;
+	char buffer1 [48];// char buffer2 [48]; //char buffer3 [48];//char * ptr = buffer;
+	uint8_t id = 0, length = 0; unsigned long ext_adress = 0; unsigned int adress = 0;
 	
 	if ((fp1 = fopen (nameoffile1, "r")) == NULL) //функция fopen возвращает указатель на файл или NULL, если не удалось открыть файл. 
 	{					//1й аргумент адрес строки, содержащий имя файла, 2й - аргумент, определяющий режим открытия файла
 	printf ("Не удаётся открыть %s", nameoffile1); //"r" - открыть тектовый файл только для чтения. 
 	exit (1);  //функция закрывает программу, аргумент передаётся ОС, 0 при корректном завершение, 1 - при аварийном.
 	}
-	fgets (buffer1, 48, fp1);
-	printf ("%s\n", buffer1);
-	length = 2 * ((buffer1 [1] - 48)*10 + (buffer1 [2] - 48));
-	id = buffer1 [8] - 48;
 	
+	while (fgets (buffer1, 48, fp1) != NULL) 
+	{
+	printf ("%s", buffer1);
+	length = 0x2 * ((buffer1 [1] - 48)*16 + (buffer1 [2] - 48));
+	id = buffer1 [8] - 48;	
 	if (id == 4)
 		{
 		for (uint8_t count = 0; count < length; count++)	
 			{
-				unsigned int mnogitel = 1;
-				for (uint8_t i = 1; i < (length-count); i++)
-					mnogitel *= 10;
-				ext_adress += (buffer1 [count+9] - 48) * mnogitel;
-			//ext_adress += ((buffer1 [count+8] - 48) * (pow (10, (length-count))));
-			printf ("%d buffer1 = %d\n",ext_adress, (buffer1 [count+9] - 48));
+			unsigned long mnogitel = 1;
+			for (uint8_t i = (length-count-1); i > 0; i--)
+				mnogitel *= 16;			
+			adress += (buffer1 [count+9] - 0x30) * mnogitel;
+			printf ("%d buffer2 = %x\n",adress, (buffer1 [count+9] - 0x30));
 			}
+		printf ("id = %x,  length = %x, adress = %x\n\n", id, length, adress);
 		}
-	printf ("id = %d,  length = %d, ext_adress = %d\n", id, length, ext_adress);
-	fgets (buffer2, 48, fp1);
-	printf ("%s\n", buffer2);
-	fgets (buffer3, 48, fp1);
-	printf ("%s\n", buffer3);
+	if (!id)
+		{
+			ext_adress = (unsigned long)(adress << 16);
+		for (uint8_t count = 0; count < 4; count++)	
+			{
+			unsigned long mnogitel = 1;
+			for (uint8_t i = 3 - count; i > 0; i--)
+				mnogitel *= 16;	
+			ext_adress += (buffer1 [count+3] - 0x30) * mnogitel;
+			}
+		printf ("id = %x,  length = %x, ext_adress = %lx\n\n", id, length, ext_adress);
+		}
+	}
 	
 	fclose (fp1); //ф-я fclose закрывает файл, очищая при этом буферы. Ф-я возвращает 0 в случае успешного закрытия или EOF в противном случае
 	
